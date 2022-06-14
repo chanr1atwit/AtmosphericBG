238e56fcf968ed860388932525042d0d0b59a8be
chanr1atwit <chanr1@wit.edu>
2022-06-14 19:13:22 -0400

    Used a set to store process names

--- SoundControl/soundController.py
+++ SoundControl/soundController.py
@@ -4,18 +4,22 @@ class soundController:
     #constructor with one param
     def __init__(self,sourceID):
        self.sourceID = sourceID
-    #reads in processID from taskmanager and adds to array
-    def detectSources():
-        f = wmi.WMI()
-        arr = []
-        for process in f.win32.Process():
-            if(len(arr) == 10):
-                return arr
-            arr += [process.ProcessID()]
-    #displays list and allows user to select app
-    def selectSources():
-        pass
     
-
+    #reads in processID from taskmanager and adds to array
+    def detectSources(self):
+        f = wmi.WMI()
+        #use a set to remove duplicate processes
+        arr = set()
+        #edit for listed executable list
+        for process in f.Win32_Process():#error str object not callable
+            # do not read in all processes, just the first ten
+            #if(len(arr) == 10):
+                #return arr
+            arr.add(process.Name)
+        return arr
+    #displays list and allows user to select app
+    def selectSources(self,arr):
+       print(str(arr))
+    
         
 
