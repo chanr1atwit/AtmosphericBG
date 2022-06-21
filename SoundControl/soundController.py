25830d596a2bc8eab9497c455a9329497d9f2752
chanr1atwit <chanr1@wit.edu>
2022-06-20 22:27:29 -0400

    soundControl
    Edited arr to be set, fixed bugs

--- SoundControl/soundController.py
+++ SoundControl/soundController.py
@@ -8,18 +8,18 @@ class soundController:
     #reads in processID from taskmanager and adds to array
     def detectSources(self):
         f = wmi.WMI()
-        #use a set to remove duplicate processes
-        arr = set()
+        #use a set to remove duplicate 
+        list = set()
         #edit for listed executable list
         for process in f.Win32_Process():
             # do not read in all processes, just the first ten
             #if(len(arr) == 10):
                 #return arr
-            arr.add(process.Name)
-        return arr
+            list.add(process.Name)
+        return list
     #displays list and allows user to select app
-    def selectSources(self,arr):
-       print(str(arr))
+    def selectSources(self,list):
+       print(str(list))
     
         
 
