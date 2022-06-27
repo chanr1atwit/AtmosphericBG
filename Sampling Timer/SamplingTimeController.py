57856297b8f227782d15424d67db0816d9a597a7
leixatwit <leix@wit.edu>
2022-06-27 18:41:34 -0400

    Fixed SamplingTimeController
    I also added a new EssentiaModel.py class.

--- Sampling Timer/SamplingTimeController.py
+++ Sampling Timer/SamplingTimeController.py
@@ -1,23 +1,20 @@
 #SamplingTimeController class, last edited 6/12/2022
+import essentia.standard as ess
+
 class SamplingTimeController:
 
-    def __init__(self,sampleTime,waitTime){
+    def __init__(self,sampleTime,waitTime):
         self.sampleTime = sampleTime
         self.waitTime = waitTime
-    }
 
-    def getSampleTime(self){
+    def getSampleTime(self):
         return self.sampleTime
-    }
 
-    def getWaitTime(self){
+    def getWaitTime(self):
         return self.waitTime
-    }
 
-    def setSampleTime(self,sampleTime){
-        return self.sampleTime = sampleTime
-    }
+    def setSampleTime(self,sampleTime):
+        self.sampleTime = sampleTime
 
-    def setWaitTime(self,waitTime){
-        return self.waitTime = waitTime
-    }
+    def setWaitTime(self,waitTime):
+        self.waitTime = waitTime
