2976378440db2f789f513f7c388fcad4139dc9b5
salterdatwit <54985588+salterdatwit@users.noreply.github.com>
2022-07-14 15:11:05 -0400

    Issue-68 - created hook, import matplotlib and randimage

---
+++ PhotoLibrary/DynamicBackgroundGeneration.py
@@ -0,0 +1,9 @@
+# Provides overhead for the randimage library by setting 
+import matplotlib as plot
+import randimage as img
+
+# tags: str[]
+# Convert tags into color maps, which will be used by randimage
+# Hook for PL
+def generateImage(tags):
+    pass
