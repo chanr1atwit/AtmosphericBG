c3415d2d4b60389fb352508d0a0f3e5cbfcb62fd
leixatwit <leix@wit.edu>
2022-07-19 18:03:15 -0400

    Create SamplingTimer.py

---
+++ Sampling Timer/SamplingTimer.py
@@ -0,0 +1,38 @@
+#SamplingTimeController class, last edited 6/12/2022
+import time
+import threading
+import essentia
+import essentia.standard
+import essentia.streaming
+
+class SamplingTimer:
+
+t1 = threading.Thread(target=sampleBPM)
+
+
+    def __init__(self,sampleTime,waitTime):
+        self.sampleTime = sampleTime
+        self.waitTime = waitTime
+
+    def getSampleTime(self):
+        return self.sampleTime
+
+    def getWaitTime(self):
+        return self.waitTime
+
+    def setSampleTime(self,sampleTime):
+        self.sampleTime = sampleTime
+
+    def setWaitTime(self,waitTime):
+        self.waitTime = waitTime
+
+    def waitTime():
+        time.sleep(waitTime)
+
+    def sampleBPM(audio):
+        #returns the
+    #def sampleTime():
+
+bool exit_flag = false
+while !exit_flag:
+    waitTime()
