f8d01c91e1f1df8fe670b493f92f3b39c3fd3d59
leixatwit <leix@wit.edu>
2022-07-19 18:03:36 -0400

    Revert "Update SamplingTimeController.py"
    This reverts commit c6550ed4e5f97bb44e208759547f8a8409c159bf.

--- Sampling Timer/SamplingTimeController.py
+++ Sampling Timer/SamplingTimeController.py
@@ -8,7 +8,7 @@ import essentia.streaming
 class SamplingTimeController:
 
 t1 = threading.Thread(target=waitTime)
-t2 = threading.Thread(target=sampleBGM)
+
 
     def __init__(self,sampleTime,waitTime):
         self.sampleTime = sampleTime
@@ -31,9 +31,7 @@ t2 = threading.Thread(target=sampleBGM)
 
     def sampleBPM(audio):
         #returns the
-
-    def sampleTime():
-        time.sleep(sampleTime)
+    #def sampleTime():
 
 bool exit_flag = false
 while !exit_flag:
