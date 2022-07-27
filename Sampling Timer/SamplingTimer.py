141675bad5754cd720cf9d9f6f2d78dfbcbed037
leixatwit <leix@wit.edu>
2022-07-27 19:16:26 -0400

    Worked on SamplingTimer.py

--- Sampling Timer/SamplingTimer.py
+++ Sampling Timer/SamplingTimer.py
@@ -1,18 +1,27 @@
-#SamplingTimeController class, last edited 6/12/2022
+#SamplingTimer class, last edited 7/25/2022
 import time
 import threading
 import essentia
 import essentia.standard as ess
 import essentia.streaming
 
+
+'''
+TASKS:
+
+
+'''
 class SamplingTimer:
 
-    def __init__(self,sampleTime,waitTime):
+    def __init__(self,sampleTime,waitTime,audio):
         self.sampleTime = sampleTime
         self.waitTime = waitTime
-        self.t1 = threading.Thread(target=self.mainSample)
-        self.loader = ess.MonoLoader
+        self.mainThread = threading.Thread(target=self.mainSample)
+        self.audio = ess.MonoLoader(filename=audio)
+        self.model = TensorflowPredictEffnetDiscogs(graphFilename="discogs-effnet-bs64-1.pb")
+        self.activations = model(self.audio)
 
+    #We need a lock
     def mainSample(self):
         self.exit_flag = False
         while not self.exit_flag:
@@ -45,7 +54,8 @@ class SamplingTimer:
     def performAnalysis(self):
         time.sleep(self.sampleTime)
 
-
-    def sampleBPM(self,audio):
+    def sampleBPM(self):
         #returns the bgm based data from essential
-        pass
+        self.rhythm_extractor = ess.RhythmExtractor2013(method="multifeature")
+        bpm, beats, beats_confidence, _, beats_intervals = rhythm_extractor(self.audio)
+        return bpm, beats, beats_confidence, _, beats_intervals
---
+++ Views/DesktopWidget.py
@@ -0,0 +1,8 @@
+#DesktopWidget class, last edited 7/26/2022
+from PyQt5 import QtWidgets as QtW
+from PyQt5.QtGui import QPainter as Qp
+from PyQt5 import QtCore as QtC
+from Views.GUI import *
+
+class DesktopWidget():
+    def __init__(self, controller):
