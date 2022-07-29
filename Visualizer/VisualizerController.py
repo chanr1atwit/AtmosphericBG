8d3477f3f5e128b03fc11f2e1a6309f5693123d9
chanr1atwit <chanr1@wit.edu>
2022-07-29 14:12:48 -0400

    Visualizer library

---
+++ Visualizer/VisualizerController.py
@@ -0,0 +1,16 @@
+#Abstract base classes
+#interface
+import sys
+from PyQt5 import Qt
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtGui import QPainter,QtGui,QPen
+
+
+class Visualizer:
+    
+    def __init__(self):
+       pass
+
+   
+    
+
