f548bcb424ea27524dd0b0512143dd750bc0b4e5
chanr1atwit <chanr1@wit.edu>
2022-07-29 18:06:16 -0400

    modified visualizer to take input from speaker

--- Visualizer/VisualizerController.py
+++ Visualizer/VisualizerController.py
@@ -6,10 +6,8 @@ from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5.QtGui import QPainter,QtGui,QPen
 
 
-class Visualizer:
     
-    def __init__(self):
-       pass
+    
 
    
     
