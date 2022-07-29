32f36117afceaafeade54aec1e201d3a6c6aad6e
MinMinQuach <98611280+MinMinQuach@users.noreply.github.com>
2022-07-29 19:28:00 -0400

    Update setSingleColor.py

--- setSingleColor.py
+++ setSingleColor.py
@@ -1,5 +1,10 @@
-COLORS = ['red']
+LENGTH = 30
+s = ShellLabStaticStrip(PORT, length=LENGTH)
+s.reset()
 
+COLORS = ['blue'] #available colors 'red','green','blue','yellow','cyan','purple','white'
+
+# show classic color
 while True:
     for c in COLORS:
         s.setColor(c, brightness=1.0)
