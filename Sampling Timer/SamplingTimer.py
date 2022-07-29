b4d6d5a3ed6a8cbd58ae1c99c2c951c0aff12905
leixatwit <leix@wit.edu>
2022-07-28 21:45:30 -0400

    Updated SamplingTimer.py

--- Sampling Timer/SamplingTimer.py
+++ Sampling Timer/SamplingTimer.py
@@ -4,7 +4,9 @@ import threading
 import essentia
 import essentia.standard as ess
 import essentia.streaming
-
+import numpy as np
+from os import getcwd
+from scipy.io.wavfile import write
 
 '''
 TASKS:
@@ -13,30 +15,41 @@ TASKS:
 '''
 class SamplingTimer:
 
-    def __init__(self,sampleTime,waitTime,audio):
+    #the audio parameter is a string path to the audio file.
+    def __init__(self,sampleTime,waitTime):
+        self.samRate = 48000
         self.sampleTime = sampleTime
         self.waitTime = waitTime
         self.mainThread = threading.Thread(target=self.mainSample)
-        self.audio = ess.MonoLoader(filename=audio)
-        self.model = TensorflowPredictEffnetDiscogs(graphFilename="discogs-effnet-bs64-1.pb")
-
+        self.model = TensorflowPredictMusiCNN(graphFilename="genre_tzanetakis-musicnn-msd-1.pb")
+        self.count = 0
+        self.offset = 0
+        self.array = np.zeros(48000*15)
 
     #We need a lock
     def mainSample(self):
         self.exit_flag = False
         while not self.exit_flag:
             self.finished = False
-            threading.Thread(target=self.timer, args=(self.getSampleTime(),))
+            threading.Thread(target=self.timer, args=(self.getSampleTime(),)).start()
+            workdone = False
             while not self.finished:
                 #Does actual sample
-                self.performAnalysis()
+                if self.offset == 14 and not workdone:
+                    self.performAnalysis()
             self.finished = False
-            threading.Thread(target=self.timer, args=(self.getWaitTime(),))
+            threading.Thread(target=self.timer, args=(self.getWaitTime(),)).start()
             while not self.finished:
                 continue
 
-    def audioOffset(self):
-        pass
+    def appendAudio(self, audioPath):
+        if self.offset == 14:
+            self.offset = 0
+        loader = ess.MonoLoader(filename=f"{audioPath}\\{self.offset}",sampleRate=self.samRate)
+        audio = loader()
+        self.array[self.offset*self.samRate:(self.offset+1)*self.samRate] = audio
+        #np.append(self.array,audio)
+        self.offset+=1
 
     def timer(self,timer):
         time.sleep(timer)
@@ -56,12 +69,14 @@ class SamplingTimer:
 
     def performAnalysis(self):
         #Uses the model
-        self.activations = model(self.audio)
+        scaled = np.int16(self.array/np.max(np.abs(self.array)) * 32767)
+        write(f"{getcwd()}\\TemporaryFiles\\audioResult.wav",self.samRate, scaled)
+        audio = ess.MonoLoader(filename=f"{getcwd()}\\TemporaryFiles\\audioResult.wav",sampleRate=self.samRate)
+        self.activations = self.model(audio)
         #time.sleep(self.sampleTime)
-
 
     def sampleBPM(self):
         #returns the bgm based data from essential
         self.rhythm_extractor = ess.RhythmExtractor2013(method="multifeature")
-        bpm, beats, beats_confidence, _, beats_intervals = rhythm_extractor(self.audio)
+        bpm, beats, beats_confidence, _, beats_intervals = rhythm_extractor(self.array)
         return bpm, beats, beats_confidence, _, beats_intervals
