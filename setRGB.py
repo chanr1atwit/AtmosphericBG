a629663e3a23cf4f38d009b84bbf81c65fa47aac
MinMinQuach <98611280+MinMinQuach@users.noreply.github.com>
2022-07-29 19:18:53 -0400

    Update setRGB.py

--- setRGB.py
+++ setRGB.py
@@ -1,19 +1,28 @@
-ls = Ws2812.LEDS(s, LENGTH )
+#setRGBColor for LED strip
+LENGTH = 30 #setLength
+s = ShellLabStrip(PORT, length=0) 
+ls = Ws2812.LEDS(s, LENGTH ) #connectLED strips
 # adjust the light power (brightness)
-ON = 255 
+ON = 255
 RED, GREEN, BLUE = ON<<16, ON<<8, ON
+all_red = [RED] * LENGTH
+all_green = [GREEN] * LENGTH
+all_blue = [BLUE] * LENGTH
 rainbow = [ RED, GREEN, RED+GREEN, BLUE, BLUE+RED, BLUE+GREEN, RED+GREEN+BLUE ]
+rainbow = rainbow * (int(LENGTH/len(rainbow))+1)  # duplicate to be large enough
 
-STEP = 30
-r, g, b = 0, 0, 0
+# test three basic colors
+ls.write(all_red)
+time.sleep(1)
+ls.write(all_green)
+time.sleep(1)
+ls.write(all_blue)
+time.sleep(1)
+
+# RGB Colors Movement
 while True:
-    for color in rainbow:
-        for i in range(STEP):
-            r = int(((color>>16) & 0xFF) * i / STEP)
-            g = int(((color>>8) & 0xFF) * i / STEP)
-            b = int(((color) & 0xFF) * i / STEP)
-            c = (r<<16) + (g<<8) + b
-            info( 'R:%d G:%d B:%d'% (r,g,b) )
-            ls.pushf( [c] )
-            time.sleep(0.1)
+    ls.write(rainbow[:LENGTH])
+    #rainbow.append(rainbow.pop(0))  # left
+    rainbow.insert(0,rainbow.pop())  # right
+    time.sleep(0.1)
 
