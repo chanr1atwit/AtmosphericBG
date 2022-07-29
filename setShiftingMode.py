48795e832848706230f2ce046f82af732c7a2462
MinMinQuach <98611280+MinMinQuach@users.noreply.github.com>
2022-07-29 19:23:02 -0400

    Update setShiftingMode.py

--- setShiftingMode.py
+++ setShiftingMode.py
@@ -1,12 +1,9 @@
+LENGTH = 30
+s = ShellLabStaticStrip(PORT, length=LENGTH)
+s.reset()
+
 COLORS = ['red','green','blue','yellow','cyan','purple','white']
-
-s.reset(freq=10)
-for i in range(10):
-    s.color('red')
-    time.sleep(0.5)
-    s.color('blue')
-    time.sleep(0.5)
-
+#setShiftingSingleColor
 s.reset()
 while True:
     r = random.randint(0,7)*32
