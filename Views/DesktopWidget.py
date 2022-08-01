01d2687cbc4c341ed081976821d9e6404a44ed19
leixatwit <leix@wit.edu>
2022-08-01 19:35:02 -0400

    Delete DesktopWidget.py

--- Views/DesktopWidget.py
+++
@@ -1,8 +0,0 @@
-#DesktopWidget class, last edited 7/26/2022
-from PyQt5 import QtWidgets as QtW
-from PyQt5.QtGui import QPainter as Qp
-from PyQt5 import QtCore as QtC
-from Views.GUI import *
-
-class DesktopWidget():
-    def __init__(self, controller):
